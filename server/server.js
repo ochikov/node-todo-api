39bb11472b894bf2481b40cc2b6f2772d71aacf0
Ognyan Chikov <o.chikov@propy.com>
2018-12-15 19:30:18 +0200

    add POST /todos route and refactor

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { Users } = require('./models/user');
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+	todo.save().then((doc) => {
+		res.send(doc)
+	}, (error) => {
+		res.status(400).send(error);
+	})
+})
+
+
+app.listen(port, () => {
+	console.log('Started the app at port: ', port);
+});
