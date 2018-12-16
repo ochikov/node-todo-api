b21b1c0368dc114197b8e3b9d808f2fbba9790e2
Ognyan Chikov <o.chikov@propy.com>
2018-12-16 18:30:14 +0200

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,17 +1,20 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
 
 const todos = [
 	{
+		_id: new ObjectID(),
 		text: 'First Test Todo'
 	},
 	{
 		text: 'Second Test Todo'
 	},
 	{
+		_id: new ObjectID(),
 		text: 'Third Test Todo'
 	}
 ];
@@ -83,4 +86,30 @@ describe('GET /todos', () => {
 			})
 			.end(done)
 	})
+});
+
+describe('GET /todos/:id', () => {
+	it('should return a todo doc', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done)
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		request(app)
+			.get(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done)
+	});
+
+	it('should return 404 for non object IDs', (done) => {
+		request(app)
+			.get(`/todos/${123}`)
+			.expect(404)
+			.end(done)
+	});
 });
