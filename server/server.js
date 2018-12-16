72255196b4ff38f7bd2434807cb5ed364904348e
Ognyan Chikov <o.chikov@propy.com>
2018-12-16 18:16:18 +0200

    Add GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -28,7 +29,22 @@ app.get('/todos', (req, res) => {
 	}, (error) => {
 		res.status(400).send(error)
 	});
-})
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send({ error: 'Invalid ID' });
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send({ error: 'There is no todo with this ID' });
+		}
+		res.send({ todo });
+	}).catch((error) => {
+		res.status(400).send({ error: 'Unable to fetch the todo' });
+	});
+});
 
 
 app.listen(port, () => {
