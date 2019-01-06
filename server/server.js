c05f05b03e080fc6b60d166d0611022c4f1e6a6a
Ognyan Chikov <o.chikov@propy.com>
2019-01-06 21:32:59 +0200

    add POST /users rout and token method

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
-var { Users } = require('./models/user');
+var { User } = require('./models/user');
 
 var port = process.env.PORT || 3000;
 
@@ -83,6 +83,20 @@ app.patch('/todos/:id', (req, res) => {
 		res.send({ todo });
 	}).catch((err) => {
 		res.status(400).send({ error: 'Unable to update the todo' });
+	})
+});
+
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(
+		body
+	)
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((error) => {
+		res.status(400).send(error)
 	})
 });
 
