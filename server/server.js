17970a51f6a36833f799bfb77fadfa5b798b3047
Ognyan Chikov <o.chikov@propy.com>
2019-01-06 22:29:58 +0200

    add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var port = process.env.PORT || 3000;
 
@@ -98,6 +99,12 @@ app.post('/users', (req, res) => {
 	}).catch((error) => {
 		res.status(400).send(error)
 	})
+});
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+	res.send(req.user);
 });
 
 
