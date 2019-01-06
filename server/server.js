ccd415d4264a6bbc26ecff368fe1d8591a003697
Ognyan Chikov <o.chikov@propy.com>
2019-01-06 14:41:09 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var { ObjectID } = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -58,6 +59,30 @@ app.delete('/todos/:id', (req, res) => {
 		res.send({ todo });
 	}).catch((error) => {
 		res.status(400).send({ error: 'Unable to fetch the todo' });
+	})
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send({ error: 'Invalid ID' });
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+		if (!todo) {
+			return res.status(404).send({ error: 'There is no todo with this ID' })
+		}
+		res.send({ todo });
+	}).catch((err) => {
+		res.status(400).send({ error: 'Unable to update the todo' });
 	})
 });
 
